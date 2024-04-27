fc49abdb47749ea415ba85eafd09ef8079b0d202
lyngutierrez <161108279+lyngutierrez@users.noreply.github.com>
2024-04-26 23:22:01 -0700

    Add files via upload

---
+++ tic tac toe/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Create a JFrame
+        JFrame frame = new JFrame("Tic Tac Toe");
+
+        // Set the size of the frame
+        frame.setSize(300, 500);
+
+
+        // Make the frame visible
+        frame.setVisible(true);
+
+        // Set default close operation
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
